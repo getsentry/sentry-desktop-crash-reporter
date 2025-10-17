b056615975168952cd5a7d30581f49cc1a11ac13
J-P Nurmi <jpnurmi@gmail.com>
2025-10-17 13:56:15 +0200

    fix: prevent close accelerator tooltip on mac
    https://learn.microsoft.com/en-us/windows/apps/design/input/keyboard-accelerators#tooltips

--- Sentry.CrashReporter/Views/MainPage.cs
+++ Sentry.CrashReporter/Views/MainPage.cs
@@ -10,70 +10,73 @@ public sealed class MainPage : Page
     public MainPage()
     {
         this.DataContext<MainViewModel>((view, vm) => view
-            .KeyboardAccelerators(GetKeyboardAccelerators())
             .NavigationCacheMode(NavigationCacheMode.Required)
             .Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
-            .Content(new LoadingView()
-                .Source(x => x.Binding(() => vm))
-                .LoadingContent(new ProgressRing()
-                    .Width(72)
-                    .Height(72))
-                .Content(new Grid()
-                    .Region(attached: true)
-                    .Padding(new Thickness(16))
-                    .RowSpacing(16)
-                    .RowDefinitions("Auto,Auto,*,Auto")
-                    .Children(
-                        new HeaderView()
-                            .Grid(row: 0),
-                        new Grid()
-                            .Grid(row: 1)
-                            .ColumnDefinitions("*,Auto")
-                            .Children(new TextBlock()
-                                    .Grid(column: 0)
-                                    .Style(ThemeResource.Get<Style>("SubtitleTextBlockStyle"))
-                                    .Text(x => x.Binding(() => vm.Subtitle)),
-                                new Segmented()
-                                    .Name(out var segmented)
-                                    .Grid(column: 1)
-                                    .SelectedIndex(x => x.Binding(() => vm.SelectedIndex).TwoWay())
-                                    .Items(
-                                        new SegmentedItem()
-                                            .Content(new FontAwesomeIcon(FA.CommentDots))
-                                            .ToolTip("Feedback")
-                                            .Navigation(request: "feedback"),
-                                        new SegmentedItem()
-                                            .Content(new FontAwesomeIcon(FA.Bug))
-                                            .ToolTip("Event")
-                                            .Navigation(request: "event"),
-                                        new SegmentedItem()
-                                            .Content(new FontAwesomeIcon(FA.Paperclip))
-                                            .ToolTip("Attachments")
-                                            .Visibility(x => x.Binding(() => vm.Attachments)
-                                                .Convert(BindingConverter.ToVisibility))
-                                            .Navigation(request: "attachment"),
-                                        new SegmentedItem()
-                                            .Content(new FontAwesomeIcon(FA.Code))
-                                            .ToolTip("Envelope")
-                                            .Navigation(request: "envelope"))),
-                        new Grid()
-                            .Region(attached: true, navigator: "Visibility")
-                            .Grid(row: 2)
+            .Content(new Grid()
+                .Children(new Control()
+                        .IsHitTestVisible(false)
+                        .KeyboardAccelerators(GetKeyboardAccelerators()),
+                    new LoadingView()
+                        .Source(x => x.Binding(() => vm))
+                        .LoadingContent(new ProgressRing()
+                            .Width(72)
+                            .Height(72))
+                        .Content(new Grid()
+                            .Region(attached: true)
+                            .Padding(new Thickness(16))
+                            .RowSpacing(16)
+                            .RowDefinitions("Auto,Auto,*,Auto")
                             .Children(
-                                new FeedbackView()
-                                    .Region(name: "feedback")
-                                    .Visibility(Visibility.Visible),
-                                new EventView()
-                                    .Region(name: "event")
-                                    .Visibility(Visibility.Collapsed),
-                                new AttachmentView()
-                                    .Region(name: "attachment")
-                                    .Visibility(Visibility.Collapsed),
-                                new EnvelopeView()
-                                    .Region(name: "envelope")
-                                    .Visibility(Visibility.Collapsed)),
-                        new FooterView()
-                            .Grid(row: 3)))));
+                                new HeaderView()
+                                    .Grid(row: 0),
+                                new Grid()
+                                    .Grid(row: 1)
+                                    .ColumnDefinitions("*,Auto")
+                                    .Children(new TextBlock()
+                                            .Grid(column: 0)
+                                            .Style(ThemeResource.Get<Style>("SubtitleTextBlockStyle"))
+                                            .Text(x => x.Binding(() => vm.Subtitle)),
+                                        new Segmented()
+                                            .Name(out var segmented)
+                                            .Grid(column: 1)
+                                            .SelectedIndex(x => x.Binding(() => vm.SelectedIndex).TwoWay())
+                                            .Items(
+                                                new SegmentedItem()
+                                                    .Content(new FontAwesomeIcon(FA.CommentDots))
+                                                    .ToolTip("Feedback")
+                                                    .Navigation(request: "feedback"),
+                                                new SegmentedItem()
+                                                    .Content(new FontAwesomeIcon(FA.Bug))
+                                                    .ToolTip("Event")
+                                                    .Navigation(request: "event"),
+                                                new SegmentedItem()
+                                                    .Content(new FontAwesomeIcon(FA.Paperclip))
+                                                    .ToolTip("Attachments")
+                                                    .Visibility(x => x.Binding(() => vm.Attachments)
+                                                        .Convert(BindingConverter.ToVisibility))
+                                                    .Navigation(request: "attachment"),
+                                                new SegmentedItem()
+                                                    .Content(new FontAwesomeIcon(FA.Code))
+                                                    .ToolTip("Envelope")
+                                                    .Navigation(request: "envelope"))),
+                                new Grid()
+                                    .Region(attached: true, navigator: "Visibility")
+                                    .Grid(row: 2)
+                                    .Children(
+                                        new FeedbackView()
+                                            .Region(name: "feedback")
+                                            .Visibility(Visibility.Visible),
+                                        new EventView()
+                                            .Region(name: "event")
+                                            .Visibility(Visibility.Collapsed),
+                                        new AttachmentView()
+                                            .Region(name: "attachment")
+                                            .Visibility(Visibility.Collapsed),
+                                        new EnvelopeView()
+                                            .Region(name: "envelope")
+                                            .Visibility(Visibility.Collapsed)),
+                                new FooterView()
+                                    .Grid(row: 3))))));
     }
 
     private static KeyboardAccelerator[] GetKeyboardAccelerators()
