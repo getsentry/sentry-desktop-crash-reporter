f15f5d74675b6f0af295d56c6775a5086c1c8568
J-P Nurmi <jpnurmi@gmail.com>
2026-01-02 16:41:57 +0100

    CI: update deps

---
+++ .github/scripts/update-uno-sdk.ps1
@@ -0,0 +1,32 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+
+$path = "$PSScriptRoot/../../global.json"
+
+function Get-Json($key)
+{
+    (Get-Content -Raw -Path $path | ConvertFrom-Json).$key
+}
+
+function Set-Json($key, $value)
+{
+    $json = Get-Content -Raw -Path $path | ConvertFrom-Json
+    $json.$key = $value
+    $json | ConvertTo-Json -Depth 10 | Set-Content -Path $path -Encoding UTF8
+}
+
+switch ("$($args[0])")
+{
+    "get-version"
+    {
+        (Get-Json 'msbuild-sdks').'Uno.Sdk'
+    }
+    "get-repo"
+    {
+        "https://github.com/unoplatform/uno"
+    }
+    "set-version"
+    {
+        Set-Json 'msbuild-sdks' @{ 'Uno.Sdk' = "$($args[1])" }
+    }
+}
